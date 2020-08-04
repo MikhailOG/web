b1a7bf35c241126c9f1764d14da90a2e6acc8743
Mikhail Popov <beto4huk@gmail.com>
2020-08-04 22:45:53 +0300

    buttons modified

--- src/Btn/Btn.js
+++ src/Btn/Btn.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const btn = (props) => {
     const style = {
-        background: '#4A4063'
+        background: "#E3D7FF",
+        border: '3px solid #f3fffd',
+        borderBottom: 'none'
     }
     return ( <div type="bed" className={props.serviceClass + " btnBed"} style={(props.hover === props.index) ? style:null}>
                 <button type="submit"
