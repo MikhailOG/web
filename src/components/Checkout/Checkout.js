687f37ca522d450fed61e2fd93d12d62a58d71fd
Mikhail Popov <beto4huk@gmail.com>
2020-11-15 22:44:07 +0300

    working on checkout design

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -5,12 +5,18 @@ class Checkout extends Component {
 
 render () {
     return (
-        <div className="checkout">
-            <h3>Congrats!</h3>
-            <canvas id="myCanvas" width={this.props.canvasSize} height={this.props.canvasSize}></canvas>
-            <Button
-            click
-            classes="escape">Click</Button>
+        <div className="checkout-wrapper">
+            <div className="checkout-border-left"/>
+            <div className="checkout-border-top"/>
+            <div className="checkout-border-bottom"/>
+            <div className="checkout-border-right"/>
+            <div className="checkout">
+                    <h3>Congrats!</h3>
+                    <canvas id="myCanvas" width={this.props.canvasSize} height={this.props.canvasSize}></canvas>
+                    <Button
+                    click
+                    classes="escape">Click</Button>
+                </div>          
         </div>
     );
 }
