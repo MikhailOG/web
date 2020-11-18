e58bbc04ace2f4f9d438a6ead6a640dffab6443d
Mikhail Popov <beto4huk@gmail.com>
2020-11-18 19:51:23 +0300

    erase function added to checkout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -2,22 +2,58 @@ import React, { Component } from 'react';
 import Button from '../UI/Button';
 
 class Checkout extends Component {
+state={
+    wrapperStyle: {},
+    checkoutStyle: {},
+    canvasStyle: {},
+    content: null
 
+}
+
+
+erase = () => {
+    this.setState({
+        wrapperStyle: {
+            background: "radial-gradient(circle at top right, transparent 39.5%, #10387d 39.5%)",
+            animation: "gradientBG 0.5s linear 1 0.0007s",
+            backgroundSize: "280% 280%",
+            backgroundPosition: "0% 100%"
+        },
+        checkoutStyle: {
+            color: "red",
+            background: "radial-gradient(circle at top right, transparent 39.5%, #10387d 39.5% 39.65%, #ABA9BF 39.65%)",
+            animation: "gradientBG 0.5s linear 1",
+            backgroundSize: "280% 280%",
+            backgroundPosition: "0% 100%"
+        },
+        canvasStyle: {
+            background: "radial-gradient(circle at top right, transparent 39.5%, #f3fffd 39.5%)",
+            animation: "gradientBG 0.5s linear 1 0.1s",
+            backgroundSize: "280% 280%",
+            backgroundPosition: "0% 100%"
+        }
+    }, console.log("clicked"))
+}
 render () {
+
+    this.setState({
+        content: 
+        <div className="checkout-wrapper" style={this.state.wrapperStyle}>
+        <div className="checkout" style={this.state.checkoutStyle}>
+            <h3>Congrats!</h3>
+            <canvas 
+                id="myCanvas" 
+                width={this.props.canvasSize} 
+                height={this.props.canvasSize}
+                style={this.state.canvasStyle}></canvas>
+            <Button
+            clicked={this.erase}
+            classes="escape">Click</Button>
+        </div>          
+    </div>
+    })
     return (
-        <div className="checkout-wrapper">
-            <div className="checkout-border-left"/>
-            <div className="checkout-border-top"/>
-            <div className="checkout-border-bottom"/>
-            <div className="checkout-border-right"/>
-            <div className="checkout">
-                    <h3>Congrats!</h3>
-                    <canvas id="myCanvas" width={this.props.canvasSize} height={this.props.canvasSize}></canvas>
-                    <Button
-                    click
-                    classes="escape">Click</Button>
-                </div>          
-        </div>
+        this.state.content
     );
 }
 }
