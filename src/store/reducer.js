5a73c798958819e83b813495aa8a2bbaf43a63b8
MikhailOG <beto4huk@gmail.com>
2020-11-27 16:13:24 +0300

    inputRow object added to the store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,7 +3,7 @@ import expect from 'expect';
 
 // reducer
 const initialState = {
-    input: [
+    inputRows: [
         {
 
         }
@@ -40,55 +40,88 @@ const reducer = (state = initialState, action) => {
 
 export default reducer;
 
-// const toggleBackdrop = (state = [], action) => {
-//     switch (action.type) {
-//         case 'WINDOW_RESIZE':
-//             console.log(action.innerWidth + ' x ' + action.innerHeight);
-//             return {
-//                 ...state,
-//                 layout: {
-//                     ...state.layout,
-//                     innerWidth: action.innerWidth,
-//                     innerHeight: action.innerHeight
-//                 }
-//             };
-//         default:
-//             return state;
-//     };
-// };
-// const testToggleBackdrop = ( ) => {
-//     const stateBefore = {
-//         input: [
-//             {
-    
-//             }
-//         ],
-//         layout: {
-//             innerWidth: 0,
-//             innerHeight: 0,
-//             showBackdrop: false
-//         }
-//     };
-//     const action = {
-//         type: 'WINDOW_RESIZE',
-//         innerWidth: 500,
-//         innerHeight: 900
-//     };
-//     const stateAfter = {
-//         input: [
-//             {
-    
-//             }
-//         ],
-//         layout: {
-//             innerWidth: 500,
-//             innerHeight: 900,
-//             showBackdrop: false
-//         }
-//         };
-//     deepFreeze(stateBefore);
-//     deepFreeze(action);
-//     expect(toggleBackdrop(stateBefore, action)).toEqual(stateAfter);
-// };
-// testToggleBackdrop();
+const addRow = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_ROW': 
+            return {
+                ...state,
+                inputRows: 
+                    [   ...state.inputRows.slice(0, action.index),
+                        {
+                            id: action.id,
+                            index: action.index,
+                            qty: 1
+                        },
+                        ...state.inputRows.slice(action.index)
+                        .map(row =>  Object.assign({}, row, {index: row.index + 1}))
+                    ]
+            };
+        default:
+            return state;
+    };
+};
+const testAddRow = ( ) => {
+    const stateBefore = {
+        inputRows: [
+            {
+                id: "CORING",
+                index: 0,
+                qty: 1
+            },
+            {
+                id: "WIRE",
+                index: 1,
+                qty: 2
+            },
+            {
+                id: "SAW",
+                index: 2,
+                qty: 4
+            }
+        ],
+        layout: {
+            innerWidth: 0,
+            innerHeight: 0,
+            showBackdrop: false
+        }
+    };
+    const action = {
+        type: 'ADD_ROW',
+        id: "NEW_CORING",
+        index: 0
+    };
+    const stateAfter = {
+        inputRows: [
+            {
+                id: "NEW_CORING",
+                index: 0,
+                qty: 1
+            },
+            {
+                id: "CORING",
+                index: 1,
+                qty: 1
+            },
+            {
+                id: "WIRE",
+                index: 2,
+                qty: 2
+            },
+            {
+                id: "SAW",
+                index: 3,
+                qty: 4
+            }
+        ],
+        layout: {
+            innerWidth: 0,
+            innerHeight: 0,
+            showBackdrop: false
+        }
+        };
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+    expect(addRow(stateBefore, action)).toEqual(stateAfter);
+};
+testAddRow();
 console.log("Test has passed")
